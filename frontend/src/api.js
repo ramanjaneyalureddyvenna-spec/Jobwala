5cb5fa84c71231e210b878c37bfc020ba3154633
ramanjaneyalureddyvenna-spec <ramanjaneyalureddyvenna@gmail.com>
2025-10-19 17:53:50 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,14 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "/api", // proxy to backend
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
+
+export default API;
 
