f4543706e836fa6940ced8b0b564b404c4814eba
ramanjaneyalureddyvenna-spec <ramanjaneyalureddyvenna@gmail.com>
2025-10-19 18:39:47 +0530

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const AdminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  name: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
